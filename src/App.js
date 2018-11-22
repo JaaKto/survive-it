768d50d408dc164ff1e2fb6de8a6c6b03b6ddaa5
JaaKto <33008680+JaaKto@users.noreply.github.com>
2018-11-22 20:41:00 +0100

    Added map, currentPosition

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { GoogleApiWrapper } from 'google-maps-react';
+import CurrentLocation from './Map';
 import './App.scss';
 
-class App extends Component {
+export class MapContainer extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <CurrentLocation centerAroundCurrentLocation google={this.props.google} >
+      </CurrentLocation>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBeuypSePUX4Er0WAo7os47hCVGuuQu3eQ'
+})(MapContainer);
