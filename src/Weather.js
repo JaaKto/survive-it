33a5082d738bd22b2b078200175cb21e0b5b25ca
JaaKto <33008680+JaaKto@users.noreply.github.com>
2018-11-23 19:40:47 +0100

    Added marking of data

--- src/Weather.js
+++ src/Weather.js
@@ -43,20 +43,39 @@ export class CurrentWeather extends Component {
   getWeather = () => {
     this.setState({ isFetching: true});
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.currentLocation.lat}&lon=${this.state.currentLocation.lng}&appid=${API_KEY}&units=metric`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.currentLocation.lat}&lon=${this.state.currentLocation.lng}&appid=${API_KEY}&lang=pl&units=metric`)
       .then(response => response.json())
       .then(json => this.setState({ currentWeather: json, isFetching: false}));
   }
 
   render() {
+
     if (this.state.currentWeather) {
       return (
         <div className='weather'>
+          <div>
+            <h1><span>Niebezpieczeństwo</span></h1>
+            <h2>Lepiej nie wychodź!</h2>
+          </div>
           <h2>{this.state.currentWeather.name}</h2>
-          <h1>{this.state.currentWeather.weather[0].main}</h1>
-          <h3>Pressure: {this.state.currentWeather.main.pressure}hPa</h3>
-          <h3>Temp: {this.state.currentWeather.main.temp}°C</h3>
-          <h3>Humidity: {this.state.currentWeather.main.humidity}%</h3>
+          <h1><span style={{
+            color: this.state.currentWeather.weather[0].id === 800 && this.state.currentWeather.weather[0].id === 801 ? 'green':
+            this.state.currentWeather.weather[0].id >= 802 ? 'yellow' : 'red'
+          }}>{this.state.currentWeather.weather[0].description}</span></h1>
+          <div>
+          <h3>Temp: <span style={{
+              color: this.state.currentWeather.main.temp >= 13 && this.state.currentWeather.main.temp <= 23 ? 'green':
+              this.state.currentWeather.main.temp >= 11 && this.state.currentWeather.main.temp <= 25 ? 'yellow' : 'red'
+            }}>{this.state.currentWeather.main.temp}°C</span></h3>
+            <h3>Ciśnienie: <span style={{
+              color: this.state.currentWeather.main.pressure >= 1000 && this.state.currentWeather.main.pressure <= 1016 ? 'green':
+              this.state.currentWeather.main.pressure >= 998 && this.state.currentWeather.main.pressure <= 1018 ? 'yellow' : 'red'
+            }}>{this.state.currentWeather.main.pressure}hPa</span></h3>
+            <h3>Wilgotność: <span style={{
+              color: this.state.currentWeather.main.humidity >= 40 && this.state.currentWeather.main.humidity <= 60 ? 'green':
+              this.state.currentWeather.main.humidity >= 38 && this.state.currentWeather.main.humidity <= 62 ? 'yellow' : 'red'
+            }}>{this.state.currentWeather.main.humidity}%</span></h3>
+          </div>
         </div>
       )
     } else {
